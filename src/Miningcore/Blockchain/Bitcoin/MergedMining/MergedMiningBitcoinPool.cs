985c7d8adf3973d47e7b09edb2a414df00e07aa9
NINJAK1DD <78038710+NINJAK1DD@users.noreply.github.com>
2026-07-03 19:12:10 +0100

    feat(merged-mining): add pool integration

---
+++ src/Miningcore/Blockchain/Bitcoin/MergedMining/MergedMiningBitcoinPool.cs
@@ -0,0 +1,85 @@
+using System.Reactive;
+using Autofac;
+using AutoMapper;
+using Microsoft.IO;
+using Miningcore.Blockchain.Bitcoin.Configuration;
+using Miningcore.Configuration;
+using Miningcore.Extensions;
+using Miningcore.JsonRpc;
+using Miningcore.Messaging;
+using Miningcore.Mining;
+using Miningcore.Nicehash;
+using Miningcore.Persistence;
+using Miningcore.Persistence.Repositories;
+using Miningcore.Stratum;
+using Miningcore.Time;
+using Newtonsoft.Json;
+
+namespace Miningcore.Blockchain.Bitcoin.MergedMining;
+
+[CoinFamily(CoinFamily.Bitcoin)]
+public class MergedMiningBitcoinPool : BitcoinPool
+{
+    public MergedMiningBitcoinPool(IComponentContext ctx,
+        JsonSerializerSettings serializerSettings,
+        IConnectionFactory cf,
+        IStatsRepository statsRepo,
+        IMapper mapper,
+        IMasterClock clock,
+        IMessageBus messageBus,
+        RecyclableMemoryStreamManager rmsm,
+        NicehashService nicehashService) :
+        base(ctx, serializerSettings, cf, statsRepo, mapper, clock, messageBus, rmsm, nicehashService)
+    {
+    }
+
+    private MergedMiningConfig mergedMiningConfig;
+
+    private bool MergedMiningEnabled => mergedMiningConfig?.Enabled == true;
+
+    public override void Configure(PoolConfig pc, ClusterConfig cc)
+    {
+        mergedMiningConfig = pc.Extra.SafeExtensionDataAs<MergedMiningPoolConfigExtra>()?.MergedMining;
+        base.Configure(pc, cc);
+    }
+
+    protected override WorkerContextBase CreateWorkerContext()
+    {
+        return new MergedMiningBitcoinWorkerContext();
+    }
+
+    protected override async Task OnAuthorizeAsync(StratumConnection connection,
+        Timestamped<JsonRpcRequest> tsRequest, CancellationToken ct)
+    {
+        if(!MergedMiningEnabled)
+        {
+            await base.OnAuthorizeAsync(connection, tsRequest, ct);
+            return;
+        }
+
+        var request = tsRequest.Value;
+        var requestParams = request.ParamsAs<string[]>();
+        var password = requestParams?.Length > 1 ? requestParams[1] : null;
+        var auxiliaryAddress = MergedMiningPasswordParser.GetValue(password,
+            mergedMiningConfig.AddressParameter);
+
+        if(string.IsNullOrWhiteSpace(auxiliaryAddress) && mergedMiningConfig.RequireAuxAddress)
+            throw new StratumException(StratumError.UnauthorizedWorker,
+                $"missing {mergedMiningConfig.AddressParameter} auxiliary address in password");
+
+        if(!string.IsNullOrWhiteSpace(auxiliaryAddress))
+        {
+            if(manager is not MergedMiningBitcoinJobManager mergedManager)
+                throw new InvalidOperationException("Merged-mining job manager is not configured");
+
+            if(!await mergedManager.ValidateAuxiliaryAddressAsync(auxiliaryAddress, ct))
+                throw new StratumException(StratumError.UnauthorizedWorker,
+                    "invalid auxiliary payout address");
+        }
+
+        var context = connection.ContextAs<MergedMiningBitcoinWorkerContext>();
+        context.AuxiliaryMiner = auxiliaryAddress;
+
+        await base.OnAuthorizeAsync(connection, tsRequest, ct);
+    }
+}
