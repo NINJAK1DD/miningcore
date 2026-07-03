a5cf4f79b773793cb0024af838e9a1119889e669
NINJAK1DD <78038710+NINJAK1DD@users.noreply.github.com>
2026-07-03 18:40:28 +0100

    test(merged-mining): enable password parser tests

--- src/Miningcore.Tests/Blockchain/Bitcoin/MergedMining/MergedMiningPasswordParserTests.cs
+++ src/Miningcore.Tests/Blockchain/Bitcoin/MergedMining/MergedMiningPasswordParserTests.cs
@@ -1,4 +1,3 @@
-#if false
 using Miningcore.Blockchain.Bitcoin.MergedMining;
 using Xunit;
 
@@ -6,5 +5,30 @@ namespace Miningcore.Tests.Blockchain.Bitcoin.MergedMining;
 
 public class MergedMiningPasswordParserTests
 {
+    [Theory]
+    [InlineData("d=65536;doge=DJ7zExampleAddress", "DJ7zExampleAddress")]
+    [InlineData(" DOGE = DJ7zExampleAddress ; d = 1024 ", "DJ7zExampleAddress")]
+    [InlineData("d=1;foo=bar;DoGe=DJ7zExampleAddress", "DJ7zExampleAddress")]
+    public void GetValue_ReturnsCaseInsensitiveTrimmedValue(string password, string expected)
+    {
+        Assert.Equal(expected, MergedMiningPasswordParser.GetValue(password, "doge"));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("d=65536")]
+    [InlineData("dogecoin=DJ7zExampleAddress")]
+    [InlineData("doge=")]
+    [InlineData("doge")]
+    public void GetValue_ReturnsNullWhenValueIsMissing(string password)
+    {
+        Assert.Null(MergedMiningPasswordParser.GetValue(password, "doge"));
+    }
+
+    [Fact]
+    public void GetValue_ReturnsFirstMatchingValue()
+    {
+        Assert.Equal("first", MergedMiningPasswordParser.GetValue("doge=first;doge=second", "doge"));
+    }
 }
-#endif
