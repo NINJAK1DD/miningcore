696d02ad750f931517fc7b6b818753f33ccbb80b
NINJAK1DD <78038710+NINJAK1DD@users.noreply.github.com>
2026-07-03 19:14:13 +0100

    Register merged-mining services

--- src/Miningcore/AutofacModule.cs
+++ src/Miningcore/AutofacModule.cs
@@ -5,6 +5,7 @@ using Miningcore.Banning;
 using Miningcore.Blockchain.Alephium;
 using Miningcore.Blockchain.Beam;
 using Miningcore.Blockchain.Bitcoin;
+using Miningcore.Blockchain.Bitcoin.MergedMining;
 using Miningcore.Blockchain.Conceal;
 using Miningcore.Blockchain.Cryptonote;
 using Miningcore.Blockchain.Equihash;
@@ -82,7 +83,8 @@ public class AutofacModule : Module
             .SingleInstance();
 
         builder.RegisterAssemblyTypes(ThisAssembly)
-            .Where(t => t.GetCustomAttributes<CoinFamilyAttribute>().Any() && t.GetInterfaces()
+            .Where(t => t != typeof(BitcoinPool) &&
+                t.GetCustomAttributes<CoinFamilyAttribute>().Any() && t.GetInterfaces()
                 .Any(i =>
                     i.IsAssignableFrom(typeof(IMiningPool)) ||
                     i.IsAssignableFrom(typeof(IPayoutHandler)) ||
@@ -188,7 +190,9 @@ public class AutofacModule : Module
         //////////////////////
         // Bitcoin and family
 
-        builder.RegisterType<BitcoinJobManager>();
+        builder.RegisterType<MergedMiningBitcoinJobManager>()
+            .As<BitcoinJobManager>()
+            .AsSelf();
 
         //////////////////////
         // Conceal
